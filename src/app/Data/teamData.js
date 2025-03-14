2487e8b9e66c6bba2e6bdc85036aa1ac128b8c6a
CB <chandrabhushankumar138@gmail.com>
2025-03-14 16:29:05 +0530

    added team member details

--- src/app/Data/teamData.js
+++ src/app/Data/teamData.js
@@ -3,8 +3,8 @@
     tech: [
       { name: `Chandrabhushan Kumar`, role: `Tech Lead & UI/UX`, description: `Full-stack developer and Specializes in intuitive interfaces with engineering precision`, git:`https://github.com/Codushan`, linked:`https://www.linkedin.com/in/chandrabhushan3/` , insta:`https://www.instagram.com/chandrabhushan_acb/` },
       { name: `Himanshu Raj`, role: `Backend Developer`, description: `Full-stack developer and Data structures & algorithms specialist with civil engineering background`, git:`https://github.com/X99ror`, linked:`https://www.linkedin.com/in/himanshu-raj-670694267?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app` , insta:`https://www.instagram.com/the_h.r.j/` },
-      { name: `Ponnala Venkata Padma Kishor`, role: `Developer`, description: `Creates responsive interfaces with architectural influence`, git:`#`, linked:`#` , insta:`#` },
-      { name: `Arun Krishnan K`, role: `Junior Developer`, description: `Creates responsive interfaces with architectural influence`, git:`#`, linked:`#` , insta:`#` },
+      { name: `Ponnala Venkata Padma Kishor`, role: `Developer`, description: `Creates responsive interfaces with architectural influence`, git:`https://github.com/Pvpkishore`, linked:`https://www.linkedin.com/in/ponnala-venkata-padma-kishor-76679326a` , insta:`https://www.instagram.com/kishore_1007_?igsh=MXYzbHY2dG0wMHEzdw==` },
+      { name: `Arun Krishnan K`, role: `Junior Developer`, description: `Creates responsive interfaces with architectural influence`, git:`https://github.com/arunkrish11`, linked:`http://www.linkedin.com/in/arunkrish11` , insta:`https://instagram.com/arunkrish.in` },
       { name: `Krishna Singh`, role: `Junior Developer`, description: `Creates responsive interfaces with architectural influence`, git:`#`, linked:`#` , insta:`#` },
       { name: `Deepak Yadav`, role: `Junior Developer`, description: `Creates responsive interfaces with architectural influence`, git:`#`, linked:`#` , insta:`#` },
     ],
