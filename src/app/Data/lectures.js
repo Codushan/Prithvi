5926e0a100861daf655d18b37a626bf1f44e79fe
X99ror <144711429+X99ror@users.noreply.github.com>
2025-03-14 20:04:21 +0530

    added team member feature and completed lecture registration

--- src/app/Data/lectures.js
+++ src/app/Data/lectures.js
@@ -2,13 +2,17 @@ const lectures = [
 
   {
     id: 1,
-    img: "",
-    title: "Coming Soon",
-    speaker: "Not disclosed",
-    organization: "NIT Clicut",
-    date: "",
-    time: "",
-    venue: ""
+    img: "/lect1.jpg",
+    title: "Civil Engineering: New Horizons and Trends",
+    speaker: "Pankaj Garg",
+    organization: "Senior Director, Head of Engineering at AtkinsReails",
+    description: "🌍✨ Discover the Future of Civil Engineering! ✨🌍. Join us for an insightful industrial lecture as we explore the latest trends, innovations, and groundbreaking advancements shaping the future of civil engineering. From cutting-edge construction techniques to sustainable solutions, we’ll dive deep into the emerging technologies revolutionizing the industry. Be a part of this transformative journey and contribute to building a smarter, greener, and more resilient world. Let’s work together to redefine the landscapes of tomorrow! 🏗🌱📈",
+    date: "22.03.2025",
+    time: "11:00 AM-01:00 PM",
+    venue: "",
+    regfee: "399" ,
+    prize: "20 KTU points",
+
   },
   
 ];
